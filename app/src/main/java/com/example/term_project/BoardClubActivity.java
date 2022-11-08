2d2dabd0a456c9cdc6e5e60b139d86ed13dd6880
Kim-Ji-Seop <76149358+Kim-Ji-Seop@users.noreply.github.com>
2022-11-08 20:54:13 +0900

    {2022.11.08} feat : * 동아리 게시판 기본틀 - 액티비티 * 게시판 목록 - 프래그먼트 * 동아리 게시판 탭 레이아웃 & 뷰페이저2 적용 * 동아리 대표사진 적용

---
+++ app/src/main/java/com/example/term_project/BoardClubActivity.java
@@ -0,0 +1,85 @@
+package com.example.term_project;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+public class BoardClubActivity extends AppCompatActivity {
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager2;
+    private ImageView imageView;
+    ClubPageAdapter adapter;
+
+    // array for tab labels
+    private final List<String> labels = Arrays.asList("커널","가온누리","언노운","딥인트");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_board_club);
+
+        // call function to initialize views
+        init();
+
+        // bind and set tabLayout to viewPager2 and set labels for every tab
+        new TabLayoutMediator(tabLayout, viewPager2, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                TextView textView = new TextView(BoardClubActivity.this);
+                textView.setText(labels.get(position));
+                textView.setGravity(Gravity.CENTER_HORIZONTAL);
+                tab.setCustomView(textView);
+            }
+        }).attach();
+
+        // 동아리 대표 사진 change event
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                switch (tab.getPosition()) {
+                    case 0:
+                        imageView.setImageResource(R.drawable.kernel);
+                        break;
+                    case 1:
+                        imageView.setImageResource(R.drawable.gaonnuri);
+                        break;
+                    case 2:
+                        imageView.setImageResource(R.drawable.unknown);
+                        break;
+                    case 3:
+                        imageView.setImageResource(R.drawable.deepint);
+                        break;
+                }
+            }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {} // 필수 메소드 (아직 구현x)
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {} // 필수 메소드 (아직 구현x)
+        });
+    }
+    private void init() {
+        // initialize imageView
+        imageView = findViewById(R.id.board_club_iv_js);
+        // initialize tabLayout
+        tabLayout = findViewById(R.id.club_content_tb_js);
+        // initialize viewPager2
+        viewPager2 = findViewById(R.id.club_content_vp_js);
+        // create adapter instance
+        adapter = new ClubPageAdapter(this);
+        // set adapter to viewPager2
+        viewPager2.setAdapter(adapter);
+    }
+}
--- app/src/main/java/com/example/term_project/BoardFragment.java
+++ app/src/main/java/com/example/term_project/BoardFragment.java
@@ -1,5 +1,6 @@
 package com.example.term_project;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,11 +9,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.card.MaterialCardView;
+
 public class BoardFragment extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_board, container, false);
+        View root = inflater.inflate(R.layout.fragment_board, container, false);
+
+        MaterialCardView materialCardView = root.findViewById(R.id.board_card_club_mcv_dong_gy);
+        materialCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(),BoardClubActivity.class);
+                startActivity(intent);
+            }
+        });
+        return root;
     }
 }
