69b9670ffd126577ba64ab6925c275229ed1c957
Kim-Ji-Seop <76149358+Kim-Ji-Seop@users.noreply.github.com>
2022-11-20 18:56:17 +0900

    {2022.11.20} feat: * 회원가입 API 적용 * 서버 반영 * Retrofit으로 서버 통신

---
+++ app/src/main/java/com/example/term_project/NetworkModule.java
@@ -0,0 +1,16 @@
+package com.example.term_project;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class NetworkModule {
+    private final static String BASE_URL = "http://www.seop.site";
+
+    public static Retrofit getRetrofit(){
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        return retrofit;
+    }
+}
